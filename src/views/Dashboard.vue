af305554c9f122c05d183ba7475aa235b72bdf22
Alvin Loh <alvinloh@alvinloh.com>
2019-10-01 17:57:32 +0800

    First Commit

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <v-app-bar>
+            <v-toolbar-title>mynow.page</v-toolbar-title>
+            <v-text-field
+                    label="Search..."
+            ></v-text-field>
+        </v-app-bar>
+
+        <v-container>
+            <v-row>
+                <v-col cols="12">
+                    Here is the dashboard page
+                </v-col>
+            </v-row>
+
+        </v-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Dashboard"
+    }
+</script>
+
+<style scoped>
+
+</style>
