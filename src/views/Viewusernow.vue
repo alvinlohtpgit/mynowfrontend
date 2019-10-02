3a41569b6ccded666a36f71e2dace64b29458e88
Alvin Loh <alvinloh@tuaspower.com.sg>
2019-10-02 17:57:28 +0800

    Testing the subdomain routing

---
+++ src/views/Viewusernow.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        this is view user
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Viewusernow",
+        mounted: function(){
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
