108018d3d9e01c1e5b2ac7e1159b90bad1dd3397
Alvin Loh <alvinloh@tuaspower.com.sg>
2019-10-03 17:58:53 +0800

    Added the subdomain routing and in the midst of doing the Viewnow component

--- src/views/Viewusernow.vue
+++ src/views/Viewusernow.vue
@@ -1,14 +1,40 @@
 <template>
     <div>
-        this is view user
+        <v-app-bar color="indigo lighten-2">
+            <v-toolbar-title class="white--text headline"><span class="font-weight-medium">MyNow</span><span class="font-weight-thin">.page</span></v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn elevation="5">
+                Get your own NOW page
+            </v-btn>
+        </v-app-bar>
     </div>
 </template>
 
 <script>
     export default {
         name: "Viewusernow",
+        data: () => {
+            return{
+                subdomainhost: '',
+                nowcontent:''
+            }
+        },
         mounted: function(){
+            var self = this;
+            // Parse the first parameter in the hostname
+            // And it should not be mynow
+            let currenthostname = window.location.hostname;
+            let subdomain = currenthostname.substr(0 , currenthostname.indexOf('.'));
+            self.subdomainhost = subdomain;
 
+            // Load the nowcontent
+            const baseURI = "http://localhost:3000/posts?author=" + subdomain;
+            this.$http.get(baseURI)
+                .then(function(response){
+
+                }).catch(function(err){
+                 console.log('Error ' + err);
+            });
         }
     }
 </script>
