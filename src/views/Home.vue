d5839a5b78d39d0d19b32aaa164b5571e3655db5
alvinloh <M8S24Vds>
2019-10-04 09:24:13 +0800

    Doing the main sign up function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <v-app-bar color="indigo lighten-2">
+      <v-toolbar-title class="white--text headline"><span class="font-weight-medium">MyNow</span><span class="font-weight-thin">.page</span></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn elevation="5">
+        Get your own NOW page
+      </v-btn>
+    </v-app-bar>
+
+    <v-container>
+      <div>
+        MyNow.page
+      </div>
+    </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
--- src/views/Viewusernow.vue
+++ src/views/Viewusernow.vue
@@ -7,6 +7,30 @@
                 Get your own NOW page
             </v-btn>
         </v-app-bar>
+
+        <v-container class="mt-10">
+            <v-row>
+                <v-col cols="8">
+                    <v-card>
+                        <v-card-text>
+                            <div v-html="nowcontent"></div>
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+                <v-col cols="4">
+                    <div class="d-flex justify-center">
+                        <v-avatar size="250">
+                            <img src="https://www.gravatar.com/avatar/b42b3e2cee3310e3e3b61a5675057488?s=250" alt="avatar" />
+                        </v-avatar>
+                    </div>
+
+                    <div class="d-flex justify-center mt-7">
+                        <span class="display-1 deep-orange--text text--darken-4">Alvin Loh</span>
+                    </div>
+
+                </v-col>
+            </v-row>
+        </v-container>
     </div>
 </template>
 
@@ -16,7 +40,8 @@
         data: () => {
             return{
                 subdomainhost: '',
-                nowcontent:''
+                nowcontent:'',
+                authorname: 'Alvin Loh'
             }
         },
         mounted: function(){
@@ -31,7 +56,7 @@
             const baseURI = "http://localhost:3000/posts?author=" + subdomain;
             this.$http.get(baseURI)
                 .then(function(response){
-
+                    self.nowcontent = response.data[0].content;
                 }).catch(function(err){
                  console.log('Error ' + err);
             });
